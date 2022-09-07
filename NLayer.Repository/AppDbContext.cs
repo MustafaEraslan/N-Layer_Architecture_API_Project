31d0039e45c43955f45f7ae055f0856975295505
Mustafa Eraslan <44713722+MustafaEraslan@users.noreply.github.com>
2022-09-07 15:28:03 +0300

    added_configuration

---
+++ NLayer.Repository/AppDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core;
+using System.Reflection;
+
+namespace NLayer.Repository
+{
+    public class AppDbContext:DbContext
+    {
+
+        public AppDbContext(DbContextOptions<AppDbContext> options): base(options) //DbContextOptions veritabanı yolunu startup dosyasından
+                                                                                    //verebilmek için yazıyoruz.
+                                                                                    //Bu dbcontext'te yaptığımız standart bir ctor
+        {
+
+        }
+
+        //Her bir etitity'e karşılık dbset oluşturulur
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductFeature> ProductFeatures { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) //model oluşurken çalıcak methodum
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
