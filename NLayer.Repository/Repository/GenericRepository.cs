d8cf3f81263a7758de1358c5c4ccd1fa58c12423
MustafaEraslan <44713722+MustafaEraslan@users.noreply.github.com>
2022-09-08 14:08:41 +0300

    addGenericRepositoryProperty

---
+++ NLayer.Repository/Repository/GenericRepository.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NLayer.Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayer.Repository.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T  : class
+    {
+        protected readonly AppDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public GenericRepository(AppDbContext context, DbSet<T> dbSet)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task AddAsync(T entity)
+        {
+
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task AddRangeAsync(IEnumerable<T> entities)
+        {
+             await _dbSet.AddRangeAsync(entities);
+        }
+
+        public async Task<bool> AnyAsync(System.Linq.Expressions.Expression<Func<T, bool>> expression)
+        {
+            return await _dbSet.AnyAsync(expression);
+        }
+
+        public IQueryable<T> GetAll(System.Linq.Expressions.Expression<Func<T, bool>> expression)
+        {
+            return _dbSet.AsNoTracking().AsQueryable(); //AsNoTracking ile EF Core dataları memoriye almasın traking yapmasın ki performanslı olmasın
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public void Remove(T entity)
+        {
+            _dbSet.Remove(entity); // remove da asyc yok çünkü db den silmez. remove dediğimizde aslında
+                                   // sadece o entity'in satete'ini delete olarak işaretliyoruz.
+
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            _dbSet.RemoveRange(entity);
+        }
+
+        public IQueryable<T> Where(System.Linq.Expressions.Expression<Func<T, bool>> expression)
+        {
+            return _dbSet.Where(expression);
+        }
+    }
+}
