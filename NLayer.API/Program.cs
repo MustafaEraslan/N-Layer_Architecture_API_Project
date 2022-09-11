b4f95d67309f7035c5868d995999923a4109c60c
Mustafa Eraslan <44713722+MustafaEraslan@users.noreply.github.com>
2022-09-11 14:39:20 +0300

    ConfigureAppsettings&Programcs

--- NLayer.API/Program.cs
+++ NLayer.API/Program.cs
@@ -1,3 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using NLayer.Repository;
+using System.Reflection;
+using NLayer.Core.UnitOfWorks;
+using NLayer.Repository.Repository;
+using NLayer.Core.Repositories;
+using NLayer.Core.Services;
+using NLayerService.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +15,24 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddScoped<IUnitOfWorks, IUnitOfWorks>();
+builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
+//builder.Services.AddScoped(typeof(IService<>), typeof(Service<>));
+
+
+
+builder.Services.AddDbContext<AppDbContext>(x =>
+{
+
+    x.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection"), option =>
+    {
+        option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name); //tip güvenli gidiyoruz.
+    });
+
+});
+
 
 var app = builder.Build();
 
